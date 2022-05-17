6d19559d7f28ea7591c400873f6b869e53c12c5e
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2022-05-16 23:46:46 -0400

    Update
    update css
    update task services
    update task structure

--- src/js/task.js
+++ src/js/task.js
@@ -2,12 +2,7 @@ const { ipcRenderer } = require('electron');
 const db = require('../js/database');
 const ssttdb = require('../js/ssttdb')
 
-const selectID = document.querySelector('#selectID')
-const boxClientOne = document.querySelector('#boxClientOne')
-const boxClientTwo = document.querySelector('#boxClientTwo')
-const showSSTT = document.querySelector('#showSSTT')
-const btnGenerate = document.querySelector('#btnGenerate')
-const newClient = []
+const selectID = document.querySelector('#selectID') /* select */
 
 db.getClients((clients) => {
     for(const client of clients){
@@ -18,6 +13,9 @@ db.getClients((clients) => {
     }
 })
 
+const boxClientOne = document.querySelector('#boxClientOne')
+const boxClientTwo = document.querySelector('#boxClientTwo')
+const btnSearch = document.querySelector('#btnSearch')
 btnSearch.addEventListener('click', () => {
     const selectValue = selectID.value
     db.getClient(selectValue, (clients) => {
@@ -25,79 +23,102 @@ btnSearch.addEventListener('click', () => {
         const templateClient = `
             <div class="control">
                 <label for="" class="label is-size-7">ID</label>
-                <input type="text" class="input is-small" disabled value="${clients.idCall}">
+                <input type="text" class="input is-small" disabled value="${clients.idCall}" id="optionIDCall">
             </div>
             <div class="control">
                 <label for="" class="label is-size-7">Teléfono</label>
-                <input type="text" class="input is-small" disabled value="${clients.phone}">
+                <input type="text" class="input is-small" disabled value="${clients.phone}" id="optionPhone">
             </div>
         `
         boxClientOne.innerHTML += templateClient
         const templateClientTwo = `
             <div class="control">
                 <label for="" class="label is-size-7">Nombre</label>
-                <input type="text" class="input is-small" disabled value="${clients.name}">
+                <input type="text" class="input is-small" disabled value="${clients.name}" id="optionName">
             </div>
             <div class="control">
                 <label for="" class="label is-size-7">Comuna</label>
-                <input type="text" class="input is-small" disabled value="${clients.comuna}">
+                <input type="text" class="input is-small" disabled value="${clients.comuna}" id="optionComuna">
             </div> 
         `
-        boxClientTwo.innerHTML +=templateClientTwo
-        const btnGenerateExtra = document.querySelector('#btnGenerateExtra')
-        btnGenerateExtra.addEventListener('click', () => {
-            const ont = document.querySelector('#ont').value
-            const nodo = document.querySelector('#nodo').value
-            const olt = document.querySelector('#olt').value
-            const clientExtraInfo = {
-                idCall: clients.idCall,
-                rut: clients.rut,
-                tel: clients.phone,
-                nom: clients.name,
-                ont: ont,
-                nodo: nodo,
-                olt: olt
-            }
-            newClient.push(clientExtraInfo)       
-        })
+        boxClientTwo.innerHTML += templateClientTwo
     })
-    boxClient.innerHTML = ''
+    boxClientOne.innerHTML = ''
+    boxClientTwo.innerHTML = ''
 })
 
+const newClient = []
+console.log('This is list new client: ', newClient.length)
+const boxTemplateClient = document.querySelector('#boxTemplateClient')
+const btnGenerateExtra = document.querySelector('#btnGenerateExtra')
+btnGenerateExtra.addEventListener('click', () => {
+    const optionIDCall = document.querySelector('#optionIDCall').value
+    const optionPhone = document.querySelector('#optionPhone').value
+    const optionName = document.querySelector('#optionName').value
+    const optionComuna = document.querySelector('#optionComuna').value
+    const optionONT = document.querySelector('#ont').value
+    const optionNODO = document.querySelector('#nodo').value
+    const optionOLT = document.querySelector('#olt').value
+    const newTemplate = {
+        optionIDCall,
+        optionPhone,
+        optionName,
+        optionComuna,
+        optionONT,
+        optionNODO,
+        optionOLT
+    }
+    newClient.push(newTemplate)
+    /* console.log('this is a newTemplateClient: ', newTemplate) */
+    const newTemplateClient = `
+        <p>ID: ${newTemplate.optionIDCall}</p>
+        <p>Nom: ${newTemplate.optionName}</p>
+        <p>Tel: ${newTemplate.optionPhone}</p>
+        <p>ONT: ${newTemplate.optionONT}</p>
+        <p>OLT: ${newTemplate.optionOLT}</p>
+        <p>Nodo: ${newTemplate.optionNODO}</p>
+        <p>Comuna: ${newTemplate.optionComuna}</p>
+    `
+    boxTemplateClient.innerHTML += newTemplateClient
+    console.log('This is list new client first insert: ', newClient.length)
+})
+
+const boxClient = document.querySelector('#boxClient')
+const showSSTT = document.querySelector('#showSSTT')
 ssttdb.getSSTTs((sstts) => {
-    console.log(sstts)
+    /* console.log(sstts) */
     for(const sstt of sstts){
         const newTemplateSSTT = `
-            <label for="" class="radio">
-                <input type="radio" name="Test" value="${sstt.description}"> ${sstt.name}
-            </label>
+            <option id="selectedTest" value="${sstt.description}">${sstt.name}</option>
         `
         showSSTT.innerHTML += newTemplateSSTT
-        const radioButtons = document.querySelectorAll('input[name="Test"]')
-        btnGenerate.addEventListener('click', () => {
-            let selectedTest;
-            let date = new Date();
-            /* console.log(radioButtons) */
-            for (const radioButton of radioButtons){
-                if(radioButton.checked){
-                    selectedTest = radioButton.value;
-                    break;
-                }
-            }
-            const prueba = {
-                prueba: selectedTest
-            }
-            console.log(selectedTest)
-            console.log(newClient)
-            const newTestTemplate = `
-                SOP ${date.toLocaleDateString()} - ${prueba.prueba}
-                ID: ${newClient[0].idCall}
-                TEL: ${newClient[0].tel}
-                NOM: ${newClient[0].nom}
-            `
-            boxClient.innerText = newTestTemplate;
-        })
     }
+})
+const btnGenerate = document.querySelector('#btnGenerate')
+/* const radioButtons = document.querySelectorAll('input[name="Test"]') */
+btnGenerate.addEventListener('click', () => {
+    let date = new Date();
+    /* console.log(radioButtons) */
+    /* for (const radioButton of radioButtons){
+        if(radioButton.checked){
+            selectedTest = radioButton.value;
+            break;
+        }
+    } */
+    const selectedTest = document.querySelector('#selectedTest').value
+    const prueba = {
+        prueba: selectedTest
+    }
+    console.log(selectedTest)
+    console.log(newClient)
+    const newTestTemplate = `
+        
+        SOP ${date.toLocaleDateString()} - ${prueba.prueba}
+        ID: ${newClient[0].optionIDCall}
+        TEL: ${newClient[0].optionPhone}
+        NOM: ${newClient[0].optionName}
+    `
+    boxClient.innerText = newTestTemplate;
 })
 
 
